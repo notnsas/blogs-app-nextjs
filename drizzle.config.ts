530df0f37d09ef607c48b849c2e36b1df63c0444
notnsas <ramapanjinararendra@gmail.com>
2026-06-20 01:18:05 +0700

    added log

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,7 @@ import * as dotenv from "dotenv"
 
 // Load .env.test in test environment, otherwise .env.local
 const envFile = process.env.NODE_ENV === "test" ? ".env.test" : ".env.local"
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 dotenv.config({ path: envFile })
 
 export default defineConfig({
